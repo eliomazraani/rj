dce8b3d6dc6a4181e17d5f22485acec193743f58
Elio <elio@stronde>
2024-09-20 00:10:21 +0300

    Desktop version

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -37,12 +37,90 @@ var videos = [
 ];
 
 var projects = [
-    "assets/img/bgRect.jpg",
-    "assets/img/bgRect.jpg",
-    "assets/img/bgRect.jpg",
-    "assets/img/bgRect.jpg",
-    "assets/img/bgRect.jpg",
-    "assets/img/bgRect.jpg",
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 1",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        arabicLyrics: false,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 2",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة. لقد تم توليد هذا النص من مولد النص العربي، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التي يولدها التطبيق.",
+        arabicLyrics: true,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 3",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        arabicLyrics: false,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 4",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة. لقد تم توليد هذا النص من مولد النص العربي، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التي يولدها التطبيق.",
+        arabicLyrics: true,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 5",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        arabicLyrics: false,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
+    {
+        projectImg: "assets/img/bgRect.jpg",
+        song: {
+            artist: "Raphael Jabbour",
+            name: "Hada Y2ella 6",
+            source: "assets/audio/Hada_Y2ella.mp3",
+        },
+        lyrics: "هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة. لقد تم توليد هذا النص من مولد النص العربي، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التي يولدها التطبيق.",
+        arabicLyrics: true,
+        story: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+        cover: "assets/img/bgSquare.png",
+        releaseDate: "June 27, 2017",
+        description: "Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Integer tincidunt. Nulla sit amet est. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc.",
+    },
 ];
 
 var gallery = [
@@ -73,18 +151,24 @@ const app = Vue.createApp({
             playClicked: false,
             listShow: false,
             time: "0:00",
+            total: "0:00",
             videosFade: false,
             projectsFade: false,
             picturesFade: false,
             imageIndex: null,
             showModal: false,
+            projectIndex: 1,
+            projectSelected: projects[1],
         };
     },
     mounted() {
+        window.vueApp = this;
+
         window.addEventListener("resize", this.updateWindowWidth);
         window.addEventListener("resize", this.updateSectionOffsets);
         window.addEventListener("scroll", this.scrollTop);
         window.addEventListener("scroll", this.scrollFade);
+        window.addEventListener("scroll", this.parallaxEffect);
 
         this.setHeight();
         this.updateSectionOffsets();
@@ -94,6 +178,10 @@ const app = Vue.createApp({
         audio.on("loadedmetadata", () => this.updateDuration());
         audio.on("timeupdate", () => this.updateTime());
         audio.on("ended", () => this.handleAudioEnded());
+
+        $("html, body").animate({
+            scrollTop: 0
+        }, 500);
         
         setTimeout(() => {
             this.isLoading = false;
@@ -110,7 +198,7 @@ const app = Vue.createApp({
         setDurations() {
             for (let index = 0; index < songs.length; index++) {
                 const track = $(`#track${index + 1}`);
-                track.on('loadedmetadata', () => {
+                track.on("loadedmetadata", () => {
                     const duration = track[0].duration;
                     const minutes = Math.floor(duration / 60);
                     const seconds = Math.floor(duration % 60);
@@ -119,12 +207,21 @@ const app = Vue.createApp({
                 });
             }
         },        
-        updateDuration() {
-            const audio = $("#currentTrack");
+        updateDuration(project = false) {
+            var audio;
+            if (!project) {
+                audio = $("#currentTrack");
+            } else {
+                audio = $("#projectTrack");
+            }
             const duration = audio[0].duration;
             const minutes = Math.floor(duration / 60);
             const seconds = Math.floor(duration % 60);
-            this.time = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            if (!project) {
+                this.time = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            } else {
+                this.total = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            }
         },        
         goToSection(name) {
             const targetElement = `#${name}`;
@@ -132,27 +229,73 @@ const app = Vue.createApp({
                 scrollTop: $(targetElement).offset().top - 55
             }, 1500);
         },
-        playMusic() {
-            this.playClicked = !this.playClicked;
-            const audio = $("#currentTrack");
+        goHome(scrollToProject = false) {
+            this.isLoading = true;
+            this.playClicked = false;
 
+            setTimeout(() => {
+                this.page = "home";
+                if (!scrollToProject) {
+                    $("html, body").animate({
+                        scrollTop: 0
+                    }, 100);
+                }
+            }, 250)
+            
+            setTimeout(() => {
+                this.setHeight();
+                this.updateSectionOffsets();
+                this.setDurations();
+                if (scrollToProject) {
+                    $("html, body").animate({
+                        scrollTop: $("#projects").offset().top - 55
+                    }, 100);
+                }
+            }, 500)
+
+            setTimeout(() => {
+                this.isLoading = false;
+                $("#music .logo").addClass("fade");
+                const audio = $("#currentTrack");
+                audio.on("loadedmetadata", () => this.updateDuration());
+                audio.on("timeupdate", () => this.updateTime());
+                audio.on("ended", () => this.handleAudioEnded());
+                audio[0].load();
+            }, 2000)
+        },
+        playMusic(type) {
+            this.playClicked = !this.playClicked;
+            const audio = $(`#${type}Track`);
+            
             if (this.playClicked) {
                 audio[0].play();
             } else {
                 audio[0].pause();
             }
         },
-        updateTime() {
-            const audio = $("#currentTrack")[0];
+        updateTime(project = false) {
+            var audio;
+            if (!project) {
+                audio = $("#currentTrack")[0];
+            } else {
+                audio = $("#projectTrack")[0];
+            }
             const minutes = Math.floor(audio.currentTime / 60);
             const seconds = Math.floor(audio.currentTime % 60);
             this.time = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-            const percentagePlayed = (audio.currentTime / audio.duration) * 100;
-            $(".timeSlider").css("width", percentagePlayed + "%");
+            if (!project) {
+                const percentagePlayed = (audio.currentTime / audio.duration) * 100;
+                $(".timeSlider").css("width", percentagePlayed + "%");
+            }
         },
-        handleAudioEnded() {
+        handleAudioEnded(project = false) {
             this.playClicked = false;
-            const audio = $("#currentTrack")[0];
+            var audio;
+            if (!project) {
+                audio = $("#currentTrack")[0];
+            } else {
+                audio = $("#projectTrack")[0];
+            }
             audio.currentTime = 0;
         },
         showList() {
@@ -164,6 +307,46 @@ const app = Vue.createApp({
             this.playClicked = false;
             this.listShow = false;
             $(".timeSlider").css("width", "0");
+        },
+        switchTrack(index) {
+            this.currentSongIndex += index;
+            if (this.currentSongIndex > this.songs.length - 1) {
+                this.currentSongIndex = 0;
+            }
+            if (this.currentSongIndex < 0) {
+                this.currentSongIndex = this.songs.length - 1;
+            }
+            this.currentSong = songs[this.currentSongIndex ];
+            this.playClicked = false;
+            this.listShow = false;
+            $(".timeSlider").css("width", "0");
+        },
+        openProject(index, fromProjectSection = false) {
+            if (fromProjectSection) {
+                this.projectIndex += index;
+            } else {
+                this.projectIndex = index;
+            }
+            this.isLoading = true;
+            this.projectSelected = this.projects[this.projectIndex];
+            this.playClicked = false;
+            this.time = "0:00";
+            
+            setTimeout(() => {
+                this.page = "project";
+                $("html, body").animate({
+                    scrollTop: 0
+                }, 100);
+            }, 500)
+
+            setTimeout(() => {
+                this.isLoading = false;
+                const audio = $("#projectTrack");
+                audio.on("loadedmetadata", () => this.updateDuration(true));
+                audio.on("timeupdate", () => this.updateTime(true));
+                audio.on("ended", () => this.handleAudioEnded(true));
+                audio[0].load();
+            }, 2000)
         },
         showImage(index, fromModal = false) {
             if (fromModal) {
@@ -207,15 +390,28 @@ const app = Vue.createApp({
             };
         },
         scrollFade() {
-            if (this.scrollPosition >= this.sectionOffsets.videos - 150) {
+            if (this.scrollPosition >= this.sectionOffsets.videos) {
                 this.videosFade = true;
             }
-            if (this.scrollPosition >= this.sectionOffsets.projects - 150) {
+            if (this.scrollPosition >= this.sectionOffsets.projects) {
                 this.projectsFade = true;
             }
-            if (this.scrollPosition >= this.sectionOffsets.pictures - 150) {
+            if (this.scrollPosition >= this.sectionOffsets.pictures) {
                 this.picturesFade = true;
             }
+        },
+        parallaxEffect() {
+            const scrollPosition = $(window).scrollTop();
+            const parallaxSpeed = 0.3; 
+            
+            $(".banner").each(function() {
+                const image = $(this).find(".banBG");
+        
+                if (image.length) {
+                    const imageOffset = scrollPosition * parallaxSpeed;
+                    image.css("transform", `translate(-50%, -${imageOffset}px)`);
+                }
+            });
         }
     },
 });
